ead02505c6ff11a928cd8551b4d0341977761e90
uni1127 <uni1127@hecto.co.kr>
2025-08-06 11:24:20 +0900

    modified:   chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 import google.generativeai as genai 
-genai.configure(auth=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 from notion_client import Client
 notion = Client(auth=st.secrets["NOTION_API_KEY"])
