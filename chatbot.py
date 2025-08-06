c6ce8b4cfdc4757677499e21e8785edb575f8e48
uni1127 <uni1127@hecto.co.kr>
2025-08-06 12:42:21 +0900

    modified:   chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,7 @@ genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 from notion_client import Client
 notion = Client(auth=st.secrets["NOTION_API_KEY"])
 
-page_id = "247d15a3f87880068380dd1b9be94fe0"
+page_id = "247d15a3-f878-8006-8380-dd1b9be94fe0"
 
 def get_page_text(page_id):
     blocks = notion.blocks.children.list(page_id=page_id)["results"]
